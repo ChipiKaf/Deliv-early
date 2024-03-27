a7525dc16c7e0fd646c917f7644aff1ce0460a89
Chipili Kafwilo <ckafwilo@gmail.com>
2024-03-27 16:11:15 +0200

    feat: :construction: Added auth
    Added login and logout using Auth0

---
+++ frontend/src/components/UsernameMenu.tsx
@@ -0,0 +1,40 @@
+import { CircleUserRound } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
+const UsernameMenu = () => {
+  const { user, logout } = useAuth0();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-gray-400 gap-2">
+        <CircleUserRound className="text-gray-400" />
+        {user?.email}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <Link to="/user-profile" className="font-bold hover:text-gray-400">
+            User Profile
+          </Link>
+        </DropdownMenuItem>
+        <Separator />
+        <DropdownMenuItem>
+          <Button className="flex flex-1 font-bold bg-gray-400"
+            onClick={() => logout()}
+          >
+            Log Out
+          </Button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UsernameMenu;
