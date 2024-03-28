173a6b294f8b484cb6a57cc9e63554ee55a68f5f
Chipili Kafwilo <ckafwilo@gmail.com>
2024-03-28 08:12:38 +0200

    feat: :construction: Connected Mongodb to auth0 user

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,6 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode
@@ -8,10 +10,10 @@ const Auth0ProviderWithNavigate: React.FC<Props> = ({ children }: Props) => {
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-
+    const navigate = useNavigate()
     if (!domain || !clientId || !redirectUri) throw new Error('Unable to initialize auth');
     const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log("USER", user);
+        navigate('/auth-callback')
     }
     return (
         <Auth0Provider domain={domain} clientId={clientId} 
